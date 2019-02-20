759e70ea1063f4c9f351c090e1d7937cd5c7249b
Halil İbrahim <47319650+hybrayhem38@users.noreply.github.com>
2019-02-20 23:12:41 +0300

    - use of third party library canceled - Built-in libraries used

--- app/src/main/java/software/ilhan/com/renkkodlar/MainActivity.java
+++ app/src/main/java/software/ilhan/com/renkkodlar/MainActivity.java
@@ -1,50 +1,67 @@
 package software.ilhan.com.renkkodlar;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.skydoves.colorpickerpreference.ColorEnvelope;
-import com.skydoves.colorpickerpreference.ColorListener;
-import com.skydoves.colorpickerpreference.ColorPickerView;
+//github description:
+// Color picker
+//-picks a color from color wheel and converts it to hex and rgb values
 
 public class MainActivity extends AppCompatActivity {
 
-    ColorPickerView colorPickerView;
+    ImageView colorWheel;
+    TextView txtResult;
+    View colorView;
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnSelect = (Button) findViewById(R.id.btnselect);
-        TextView textView2 = (TextView) findViewById(R.id.textView2);
 
-        final ColorEnvelope colorEnvelope = null;
-        btnSelect.setOnClickListener(new View.OnClickListener(){
+        colorWheel = findViewById(R.id.colorWheel);
+        colorView = findViewById(R.id.colorView);
+        txtResult = findViewById(R.id.txtResult);
+
+        colorWheel.setDrawingCacheEnabled(true);
+        colorWheel.buildDrawingCache(true);
+
+        //colorWheel on touch listener
+        colorWheel.setOnTouchListener(new View.OnTouchListener(){
             @Override
-            public void onClick(View v) {
-//                ColorEnvelope colorEnvelope;
-                TextView textView2 = (TextView) findViewById(R.id.textView2);
-                if (colorEnvelope.getColorHtml() != null) {
-                    textView2.setText(colorEnvelope.getColorHtml());
+            public boolean onTouch(View v, MotionEvent event){
+                if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+                    bitmap = colorWheel.getDrawingCache();
+
+                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    String hex = "#" + Integer.toHexString(pixel);
+
+
+
+                    colorView.setBackgroundColor(Color.rgb(r,g,b));
+
+                    txtResult.setText("HEX: "+hex+"\nRGB: "+r+", "+g+", "+b);
+
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"java.lang.String com.skydoves.colorpickerpreference.ColorEnvelope.getColorHtml()' on a null object reference", Toast.LENGTH_LONG).show();
-                }
+                return true;
             }
+
         });
-
-        /*colorPickerView.setColorListener(new ColorListener() {
-            @Override
-            public void onColorSelected(ColorEnvelope colorEnvelope) {
-                    LinearLayout linearLayout = findViewById(R.id.linearLayout);
-                    linearLayout.setBackgroundColor(colorEnvelope.getColor());
-            }
-        });*/
 
     }
 }
+
