592dcb628bad5e29007c634e1268092a10167f09
Halil İbrahim <47319650+hybrayhem38@users.noreply.github.com>
2019-02-23 23:14:23 +0300

    Color Picker V1.1 (Wifi) by Halil İbrahim iLHAN Release notes: -added sending hex with tcp for IOT rgb led control

--- app/src/main/java/software/ilhan/com/renkkodlar/MainActivity.java
+++ app/src/main/java/software/ilhan/com/renkkodlar/MainActivity.java
@@ -2,17 +2,28 @@ package software.ilhan.com.renkkodlar;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 //github description:
 // Color picker
 //-picks a color from color wheel and converts it to hex and rgb values
@@ -24,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
     TextView txtResult;
     View colorView;
 
+    EditText txtAddress;
+    Socket myAppSocket = null;
+    public static String wifiModuleIp = "";
+    public static int wifiModulePort = 0;
+    public static String CMD = "0";
+
     Bitmap bitmap;
 
     @Override
@@ -31,16 +48,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        colorWheel = findViewById(R.id.colorWheel);
-        picker = findViewById(R.id.picker);
-        colorView = findViewById(R.id.colorView);
-        txtResult = findViewById(R.id.txtResult);
+        colorWheel = (ImageView) findViewById(R.id.colorWheel);
+        picker = (ImageView) findViewById(R.id.picker);
+        colorView = (View) findViewById(R.id.colorView);
+        txtResult = (TextView) findViewById(R.id.txtResult);
 
         colorWheel.setDrawingCacheEnabled(true);
         colorWheel.buildDrawingCache(true);
 
+        txtAddress = (EditText) findViewById(R.id.txtAddress);
+
         final float[] currentX = {466};
         final float[] currentY = {466};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        /*final String[] ip_address = {""};
+
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText txtAddress = findViewById(R.id.txtAddress);
+
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("Tamam", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                ip_address[0] = txtAddress.getText().toString();
+                Log.d("input_control", "ip adresi: "+ip_address[0]);
+            }
+        });
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
         TranslateAnimation animation = new TranslateAnimation(currentX[0], 0, currentY[0], 0);
         animation.setFillAfter(true);
@@ -83,6 +126,17 @@ public class MainActivity extends AppCompatActivity {
                         colorView.setBackgroundColor(Color.rgb(r, g, b));
 
                         txtResult.setText("HEX: " + hex + "\nRGB: " + r + ", " + g + ", " + b);
+                        String old_hex = "";
+                        //TODO "is hex changed" not working, needs to be right coding
+                        if (!hex.equals(old_hex)) {
+                            Log.d("hex_change_test", "old_hex: " + old_hex + ", new_hex: " + hex);
+                            getIPandPort();
+                            CMD = hex + ".";
+                            Socket_AsyncTask rgb = new Socket_AsyncTask();
+                            rgb.execute();
+                            Log.d("cmd", "HEX String: " + CMD);
+                            old_hex = hex;
+                        }
                     }
                 }
                 return true;
@@ -102,6 +156,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void getIPandPort()   // getIPandPort(String[] ip_address)
+    {
+        String iPandPort = txtAddress.getText().toString();   // String iPandPort = ip_address[0];
+        Log.d("MYTEST","IP String: "+ iPandPort);
+        String temp[]= iPandPort.split(":");
+        wifiModuleIp = temp[0];
+        wifiModulePort = Integer.valueOf(temp[1]);
+        Log.d("MY TEST","IP:" +wifiModuleIp);
+        Log.d("MY TEST","PORT:"+wifiModulePort);
+    }
+    public class Socket_AsyncTask extends AsyncTask<Void,Void,Void> {
+        Socket socket;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
+                socket = new java.net.Socket(inetAddress, MainActivity.wifiModulePort);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeBytes(CMD);
+                dataOutputStream.close();
+                socket.close();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
 
 /*centreX=imageView.getX() + imageView.getWidth()  / 2;
